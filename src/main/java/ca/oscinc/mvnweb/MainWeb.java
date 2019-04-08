fc0860efe15753cabbe45e2fcee0d52c8b2db16b
Eduard Tita <eduard.tita@objectscape.ca>
2019-04-08 09:01:38 -0400

    First commit

---
+++ src/main/java/ca/oscinc/mvnweb/MainWeb.java
@@ -0,0 +1,16 @@
+package ca.oscinc.mvnweb;
+
+import ca.oscinc.mnv.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainWeb
+{
+    public static void main( String[] args )
+    {
+        App app = new App();
+        System.out.println( "Hello World!" );
+    }
+}
